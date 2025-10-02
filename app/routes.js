48f589105d32e722d032a315d63ba9865a1ed593
chague1 <c.hague1@nhs.net>
2025-10-02 15:21:15 +0000

    Added human answer route

--- app/routes.js
+++ app/routes.js
@@ -5,4 +5,27 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
-module.exports = router
+// Called when answering question about whether user is human
+router.post('/human-answer', (req, res) => {
+
+  // Make a variable and give it the value from 'human-y-n'
+  const symptoms = req.session.data.human-y-n
+
+  // Check whether the variable matches a condition (human)
+  if (symptoms === 'yes' || symptoms === 'not sure'  ) {
+
+    // Send user to the next question page about age
+    res.redirect('/age')
+
+  } else if (symptoms === 'no') {
+
+    // Send user to the Not eligible page
+    res.redirect('/not-eligible')
+
+  } else {
+
+    // Send user back to the question page
+    res.redirect('/start-page')
+
+  }
+})
